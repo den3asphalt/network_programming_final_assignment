2770a55d6cf5b7f06008be4d4c47d757af37e945
den3asphalt <86176261+den3asphalt@users.noreply.github.com>
2024-07-29 03:06:42 +0900

    完成を確認

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import discord
 import configparser
 import logging
 import requests
-import json
 import datetime
+from bs4 import BeautifulSoup
 
 
 # 設定ファイルの読み込み
@@ -29,17 +29,41 @@ url_bus = "http://bus.shibaura-it.ac.jp/db/bus_data.json"
 # バスのデータ入手
 def get_data() -> dict:
     """
-        バスのデータを取得する関数
+        バスのデータを取得する関数\n主に通信部分
 
         Returns
         -------
         dict
             バスのデータ
     """
-    response = requests.get(url_bus)            # データ取得
-    data = response.json()                      # json形式に変換
-    json_data = json.dumps(data, indent=4, ensure_ascii=False)  # json形式に変換
-    return json_data
+    response:requests.Response = requests.get(url_bus)            # データ取得
+    json_data:dict = response.json()                      # json形式で読み込み
+
+    today_URL = "http://bus.shibaura-it.ac.jp/ts/today_sheet.php"
+    response = requests.get(today_URL)            # データ取得
+
+    # 今日のバスのデータのタイトルを取得する
+    soup = BeautifulSoup(response.text, "html5lib")
+    title_tag = soup.select_one("body > div.content.ts > h2")
+
+    # アクセスできなかった場合？
+    if title_tag is None:
+        raise Exception("今日のバスのデータが取得できませんでした")
+    
+    # タイトルの取得
+    title:str = title_tag.text
+
+    time_sheet:list = json_data["timesheet"]
+
+    # 返却するデータ
+    data:dict
+
+    for i in time_sheet:
+        if i["title"].replace(" ", "") == title.replace(" ", ""):
+            data = i["list"]
+            break
+
+    return data
 
 async def bus(flag:bool,
               now_hour:int = datetime.datetime.now().hour, 
@@ -65,11 +89,13 @@ async def bus(flag:bool,
         次のバスのデータ
     """
 
+
+
     # 返却するデータ
     mes:str = ""
 
     
-    all_data:dict = calender["list"]    # 全てのバスデータ
+    all_data:dict = calender    # 全てのバスデータ
     hour_data:dict                      # 時間ごとのデータ
     bus_data:dict                       # バスのデータ  
     
@@ -92,7 +118,7 @@ async def bus(flag:bool,
         bus_data = hour_data["bus_right"]
 
     # 間隔を狭めている時のデータ
-    memo:str = hour_data["memo1"]
+    memo:str = bus_data["memo1"]
 
     # 間隔を狭めているとき
     if "間隔を狭めて" in memo:
@@ -114,9 +140,14 @@ async def bus(flag:bool,
         # 今の分が間隔を狭めている時間内の場合
         if before <= now_minute and now_minute <= after:
             return memo
-
-    # 間隔を狭めていない時 or 狭めている範囲ではない時
-    min_data:list = [int(i) for i in bus_data["num2"].split(".")]
+        
+        # 狭めている範囲ではない時
+        min_data:list = [int(i) for i in bus_data["num2"].split(".")]
+    else:
+        # 間隔を狭めていない時
+        min_data:list = [int(i) for i in bus_data["num1"].split(".")]
+     
+    
 
     minute = 0
 
@@ -131,42 +162,55 @@ async def bus(flag:bool,
 
     # メッセージの作成
     if flag:
-        mes = f"次の大学行きバスは{minute}分です"
+        mes = f"次の大学行きバスは{now_hour}:{minute}です"
     else:
-        mes = f"次の駅行きバスは{minute}分です"
+        mes = f"次の駅行きバスは{now_hour}:{minute}です"
 
     return mes
 
-# 起動時に動作する処理
+
 @client.event
 async def on_ready() -> None:
+    """
+        Botが起動したときに動作する関数
+    """
+
     print("Discord Bot is ready...")
     # コマンドの登録
     await commands.sync()
 
-# univコマンド実行時の処理
+
 @commands.command(name="univ", description="次の大学行きバスを取得します")
 async def univ(interaction: discord.Interaction) -> None:
+    """
+        univコマンド実行時の処理
+    """
     
     # メッセージの作成
     mes = await bus(True)
 
-    # メッセージの送信
-    await interaction.response.send_message(mes)
+    # メッセージの送信 ephermal=Trueで自分だけに見える
+    await interaction.response.send_message(mes, ephemeral=True)
 
 
-# statコマンド実行時の処理
 @commands.command(name="stat", description="次の駅行きバスを取得します")
 async def stat(interaction: discord.Interaction) -> None:
-    
+    """
+        statコマンド実行時の処理
+    """
+
     # メッセージの作成
     mes = await bus(False)
 
-    # メッセージの送信
-    await interaction.response.send_message(mes)
+    # メッセージの送信 ephermal=Trueで自分だけに見える
+    await interaction.response.send_message(mes, ephemeral=True)
 
 # bot起動時にバスのデータ読み込み
 calender = get_data()
 
-client.run(config_ini['DISCORD']['TOKEN'], log_handler=handler)
+# Botのトークン
+TOKEN = config_ini['DISCORD']['TOKEN']
+
+# Botの起動
+client.run(TOKEN, log_handler=handler)
 
